18db3d2943958fb88810f60cbec49a32b4a84997
vitalii hribonkin <vitalikgribenkin@gmail.com>
2025-04-23 13:54:01 +0200

    nodejs-hw-mongodb

--- src/services/contact.js
+++ src/services/contact.js
@@ -2,10 +2,9 @@ import { sortList } from "../constants/index.js";
 import ContactsCollection from "../db/Models/Contacts.js";
 import { calculatePaginationData } from "../utils/calculatePaginationData.js";
 
-// Отримання контактів для конкретного користувача з пагінацією
+
 export const getContact = async ({ userId, page = 1, perPage = 10, sortBy = '_id', sortOrder = sortList[0] }) => {
   const skip = (page - 1) * perPage;
-
   const filter = { userId };
 
   const data = await ContactsCollection.find(filter)
@@ -13,8 +12,7 @@ export const getContact = async ({ userId, page = 1, perPage = 10, sortBy = '_id
     .limit(perPage)
     .sort({ [sortBy]: sortOrder });
 
-  const totalItems = await ContactsCollection.countDocuments(filter); 
-
+  const totalItems = await ContactsCollection.countDocuments(filter);
   const paginationData = calculatePaginationData({ page, perPage, totalItems });
 
   return {
@@ -26,33 +24,33 @@ export const getContact = async ({ userId, page = 1, perPage = 10, sortBy = '_id
   };
 };
 
-// Отримання контакту за ID для конкретного користувача
-export const getContactById = async (userId, id) => {
-  return ContactsCollection.findOne({ _id: id, userId }); // фільтрація по userId
+
+export const getContactById = async (userId, contactId) => {
+  return ContactsCollection.findOne({ _id: contactId, userId });
 };
 
-// Створення нового контакту
+
 export const createContact = async (userId, payload) => {
-  const contact = await ContactsCollection.create({ ...payload, userId }); // додаємо userId
+  const contact = await ContactsCollection.create({ ...payload, userId });
   return contact;
 };
 
-// Видалення контакту для конкретного користувача
+
 export const deleteContact = async (userId, contactId) => {
-  const contact = await ContactsCollection.findOneAndDelete({ _id: contactId, userId }); // фільтрація по userId
-  return contact;
+  return ContactsCollection.findOneAndDelete({ _id: contactId, userId });
 };
 
-// Оновлення контакту для конкретного користувача
+
+
 export const updateContact = async (userId, contactId, payload, options = {}) => {
   const rawResult = await ContactsCollection.findOneAndUpdate(
-    { _id: contactId, userId }, // фільтрація по userId
+    { _id: contactId, userId },
     payload,
     {
       new: true,
       includeResultMetadata: true,
       ...options,
-    },
+    }
   );
 
   if (!rawResult || !rawResult.value) return null;
